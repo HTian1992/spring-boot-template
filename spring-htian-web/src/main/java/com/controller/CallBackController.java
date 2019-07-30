eaf186bbc98f22272c42dba8941827b8cfeb76ee
zehao.li <zehao.li@wehotelglobal.com>
2019-07-30 18:00:46 +0800

    添加list

--- spring-htian-web/src/main/java/com/controller/CallBackController.java
+++ spring-htian-web/src/main/java/com/controller/CallBackController.java
@@ -3,15 +3,23 @@ package com.controller;
 import com.base.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,8 +42,8 @@ public class CallBackController {
     private RestHighLevelClient restClient;
 
     @ApiOperation(value = "查询")
-    @RequestMapping(value = "/get", method = RequestMethod.GET)
-    public Result<Map<String, Object>> get(@RequestParam String index ,@RequestParam String type,@RequestParam String id) throws Exception {
+    @RequestMapping(value = "/load", method = RequestMethod.GET)
+    public Result<Map<String, Object>> load(@RequestParam String index ,@RequestParam String type,@RequestParam String id) throws Exception {
         GetRequest request = new GetRequest(index,type,id);
         GetResponse response = restClient.get(request);
         return Result.success(response.getSource());
@@ -43,12 +51,60 @@ public class CallBackController {
 
     @ApiOperation(value = "更新")
     @RequestMapping(value = "/update", method = RequestMethod.GET)
-    public Result<UpdateResponse> test(@RequestParam String index ,@RequestParam String type,@RequestParam String id,@RequestParam String desc) throws Exception {
+    public Result<UpdateResponse> update(@RequestParam String index ,@RequestParam String type,@RequestParam String id,@RequestParam String desc) throws Exception {
         Map<String,Object> doc = new HashMap<>();
         doc.put("desc",desc);
         UpdateRequest request = new UpdateRequest(index,type,id);
         request.doc(doc);
         UpdateResponse updateResponse = restClient.update(request);
         return Result.success(updateResponse);
+    }
+
+    @ApiOperation(value = "新增")
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public Result<IndexResponse> add(@RequestParam String index , @RequestParam String type, @RequestParam String id, @RequestParam String desc) throws Exception {
+        Map<String,Object> doc = new HashMap<>();
+        doc.put("name","遂溪张学友");
+        doc.put("age","28");
+        doc.put("desc",desc);
+        doc.put("birthday","1992-12-11");
+        IndexRequest request = new IndexRequest(index, type, id);
+        request.source(doc);
+        IndexResponse response = restClient.index(request);
+        return Result.success(response);
+    }
+
+    @ApiOperation(value = "删除")
+    @RequestMapping(value = "/delete", method = RequestMethod.GET)
+    public Result<DeleteResponse> delete(@RequestParam String index , @RequestParam String type, @RequestParam String id, @RequestParam String desc) throws Exception {
+        DeleteRequest request = new DeleteRequest(index,type,id);
+        DeleteResponse response = restClient.delete(request);
+        return Result.success(response);
+    }
+
+    @ApiOperation(value = "列表")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public Result<SearchHits> heighLoad(@RequestParam String index , @RequestParam String type, @RequestParam String id) throws Exception {
+        SearchRequest request = new SearchRequest(index);
+        request.indices();
+        SearchHits response = restClient.search(request).getHits();
+        return Result.success(response);
+    }
+
+    @ApiOperation(value = "关键字搜索")
+    @RequestMapping(value = "/keyword", method = RequestMethod.GET)
+    public Result<SearchResponse> keyword(@RequestParam String index, @RequestParam String type, @RequestParam String keyword) throws Exception {
+
+        SearchRequest request = new SearchRequest();
+        //索引
+        request.indices(index).types(type);
+        //各种组合条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.explain(false).profile(false).from(0).size(10)
+                //.postFilter(QueryBuilders.termQuery("name",keyword))
+                .postFilter(QueryBuilders.wildcardQuery("desc",keyword));
+        request.source(sourceBuilder);
+        SearchResponse response = restClient.search(request);
+        return Result.success(response);
     }
 }
