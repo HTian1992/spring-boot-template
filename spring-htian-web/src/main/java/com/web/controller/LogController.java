c0ff2de6ee9a7337788ba9c29398945979c90b2a
zehao.li <zehao.li@wehotelglobal.com>
2019-08-12 15:40:46 +0800

    test-format

--- spring-htian-web/src/main/java/com/web/controller/LogController.java
+++ spring-htian-web/src/main/java/com/web/controller/LogController.java
@@ -1,7 +1,10 @@
 package com.web.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.bean.comment.CommentListVO;
+import com.dao.entity.BComment;
 import com.dao.entity.BLog;
+import com.dao.mapper.BCommentMapper;
 import com.dao.mapper.BLogMapper;
 import com.web.base.Result;
 import io.swagger.annotations.Api;
@@ -13,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author lizehao
@@ -26,6 +32,8 @@ public class LogController {
 
     @Autowired
     private BLogMapper bLogMapper;
+    @Autowired
+    private BCommentMapper commentMapper;
 
 
     @ApiOperation(value = "")
@@ -35,5 +43,32 @@ public class LogController {
         return Result.success(a);
     }
 
+    @ApiOperation(value = "查询文章评论")
+    @RequestMapping(value = "/getCommentList", method = RequestMethod.GET)
+    public Result<List<CommentListVO>> getCommentList(@RequestParam Long blogId) {
+        List<BComment> sourceList = commentMapper.selectList(new QueryWrapper<BComment>().eq("blog_id",blogId).orderByAsc("flag_id","offset"));
+        return Result.success(genarateComment(sourceList));
+    }
+
+    public static List<CommentListVO> genarateComment( List<BComment> sourceList) {
+
+        //数据集处理
+        Map<Integer,CommentListVO> map = new HashMap<>();
+        int floor = 0;
+        for(BComment index: sourceList){
+            CommentListVO commentListVO = map.get(floor);
+            if(null == commentListVO || 1 == index.getIsRoot()){
+                floor++;
+                commentListVO = new CommentListVO();
+                commentListVO.setFloor(floor);
+            }
+            commentListVO.getComments().add(index);
+            map.put(floor,commentListVO);
+        }
+
+        //返回前端VO
+        List<CommentListVO> comments = map.values().stream().collect(Collectors.toList());
+        return comments;
+    }
 
 }
