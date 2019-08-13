69e929c202aa2f3e736cd1d049c904bdbb4a1df4
zehao.li <zehao.li@wehotelglobal.com>
2019-08-13 09:19:57 +0800

    添加spring mvc跨域访问支持。注：虽后端接口未限制访问，但浏览器基于同源策略，会把接口域名与前端服务所在域 作比较， 非同源会截断处理

---
+++ spring-htian-web/src/main/java/com/web/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * web 配置类
+ *
+ * @author lizehao
+ */
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+    /**
+     * 增加对跨域对支持
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
